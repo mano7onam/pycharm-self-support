7894f57aefc557e8145bc78341459774bcf4f9cd
andrey.matveev <amatveev@jetbrains.com>
2022-11-04 16:25:58 +0200

    Second commit

--- check_annotate_generic_self.py
+++ check_annotate_generic_self.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Self
 
 
@@ -11,3 +12,24 @@ class B:
     def foo(self: Self) -> list[Self]:
         res = list[self, self]
         return res
+
+
+class C:
+    def foo(self: Self) -> Self:
+        return C()
+
+
+class D:
+    def bar(self) -> Self:
+        ...
+
+    def foo(self: Self) -> Self:
+        return self.bar()
+
+
+class E:
+    def bar(self) -> E:
+        ...
+
+    def foo(self: Self) -> Self:
+        return self.bar()
